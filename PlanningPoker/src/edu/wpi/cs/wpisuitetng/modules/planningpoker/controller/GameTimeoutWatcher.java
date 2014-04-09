d1149b714b8ebf979b40846e1c8ce7100de1f27f
Daniel Beckwith <djbsnx@gmail.com>
2014-04-08 22:16:17 -0400

    Adds fix for games without deadlines

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GameTimeoutWatcher.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GameTimeoutWatcher.java
@@ -7,9 +7,11 @@ import java.util.Timer;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel;
 
 /**
- * The purpose of this class is to schedule tasks in the future for checking games to see if they have passed their end date.
+ * The purpose of this class is to schedule tasks in the future for checking
+ * games to see if they have passed their end date.
+ * 
  * @author dbeckwith
- *
+ * 
  */
 public class GameTimeoutWatcher {
     
@@ -33,18 +35,22 @@ public class GameTimeoutWatcher {
     
     /**
      * Adds a game to be watched for completion at its end date.
+     * 
      * @param game
      */
     public void watchGame(GameModel game) {
-        GameTimeoutTask task = new GameTimeoutTask(game);
-        // schedule with timer to run 500 ms after end time
-        timer.schedule(task, new Date(game.getEndTime().getTime() + 500));
-        // add to list of active tasks
-        activeTasks.add(task);
+        if (game.getEndTime() != null) {
+            GameTimeoutTask task = new GameTimeoutTask(game);
+            // schedule with timer to run 500 ms after end time
+            timer.schedule(task, new Date(game.getEndTime().getTime() + 500));
+            // add to list of active tasks
+            activeTasks.add(task);
+        }
     }
     
     /**
      * Stop watching a game and forget about it.
+     * 
      * @param game
      */
     public void stopWatchingGame(GameModel game) {
