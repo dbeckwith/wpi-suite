7077f220f0583c9566522c4c0a1541110b113c3e
andrewwhan <awhan@wpi.edu>
2014-04-14 19:19:03 -0400

    Code formatting

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/OptionPane.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/OptionPane.java
@@ -1,7 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker;
 
 import java.awt.Component;
 
+/**
+ * OptionPane interface allows testing to simulate yes or no selections on
+ * confirmation dialogs.
+ * 
+ * @author Andrew
+ */
 public interface OptionPane {
     /**
      * @see JOptionPane#showConfirmDialog(Component, Object, String, int);
