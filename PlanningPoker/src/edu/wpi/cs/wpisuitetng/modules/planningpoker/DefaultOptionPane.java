bc5d8a8d2eebd3c5b249e58f635f1b8cff9f5b99
andrewwhan <awhan@wpi.edu>
2014-04-14 19:19:03 -0400

    Code formatting

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/DefaultOptionPane.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/DefaultOptionPane.java
@@ -1,9 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker;
 
 import java.awt.Component;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Default implementation of OptionPane that just refers to JOptionPane
+ * 
+ * @author Andrew
+ *
+ */
 public class DefaultOptionPane implements OptionPane {
     
     @Override
