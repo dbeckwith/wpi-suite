a5fddb1164b1d03f82dbc40e93d2e58d72754f3b
andrewwhan <awhan@wpi.edu>
2014-04-19 18:54:07 -0400

    Fixes GameTest to accomodate serverside game timeout.

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameTest.java
@@ -12,11 +12,16 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.model;
 
 import java.util.Date;
+import java.util.HashSet;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.wpi.cs.wpisuitetng.Session;
+import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
+import edu.wpi.cs.wpisuitetng.modules.core.models.Project;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel.GameStatus;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel.GameType;
 import edu.wpi.cs.wpisuitetng.network.Network;
@@ -27,21 +32,47 @@ import edu.wpi.cs.wpisuitetng.network.configuration.NetworkConfiguration;
  * 
  */
 public class GameTest {
+    
+    static Session defaultSession;
+    static GameEntityManager manager;
+    static User existingUser;
+    static Project testProject;
+    static String mockSsid;
+    static MockData db;
 
     @BeforeClass
     static public void setUpBeforeClass() {
         Network.initNetwork(new MockNetwork());
         Network.getInstance().setDefaultNetworkConfiguration(
                 new NetworkConfiguration("http://wpisuitetng"));
+        mockSsid = "abc123";
+        testProject = new Project("test", "1");
+        existingUser = new User("joe", "joe", "1234", 2);
+        defaultSession = new Session(existingUser, testProject, mockSsid);
+        db = new MockData(new HashSet<Object>());
+        db.save(existingUser);
+        manager = new GameEntityManager(db);
     }
     
 	@Test
 	public void TestRequirementEndsAfterDeadline() {
 		GameModel testgame = new GameModel("Test Game", "something", null,
 				DeckListModel.getInstance().getDefaultDeck(), new Date(
-						System.currentTimeMillis() - 100000),
+						System.currentTimeMillis() + 1000),
 				GameType.DISTRIBUTED, GameStatus.PENDING);
-		Assert.assertTrue(testgame.isEnded());
+		GameModel created = new GameModel();
+		try {
+            created = manager
+                    .makeEntity(defaultSession, testgame.toJSON());
+        }
+        catch (WPISuiteException e1) {
+        }
+		try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+        }
+		Assert.assertTrue(created.isEnded());
 	}
 
 	@Test
