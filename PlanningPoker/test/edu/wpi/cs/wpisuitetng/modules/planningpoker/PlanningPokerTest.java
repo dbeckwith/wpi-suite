9cb50669266daf192597ee50afd3b2078e90abec
andrewwhan <awhan@wpi.edu>
2014-04-20 18:23:03 -0400

    CodePro for PlanningPokerTest

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/PlanningPokerTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/PlanningPokerTest.java
@@ -21,23 +21,27 @@ import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.configuration.NetworkConfiguration;
 
 /**
+ * Tests the PlanningPoker class
  * 
- * @author Andrew
- * 
+ * @author Team 9
+ * @version 1.0
  */
 public class PlanningPokerTest {
     
+    /**
+     * Tests the elements of the planning poker class (mainview, toolbarview)
+     */
     @Test
     public void testElements() {
         Network.initNetwork(new MockNetwork());
         Network.getInstance().setDefaultNetworkConfiguration(
                 new NetworkConfiguration("http://wpisuitetng"));
-        PlanningPoker poker = new PlanningPoker();
-        ViewController vc = PlanningPoker.getViewController();
+        final PlanningPoker poker = new PlanningPoker();
+        final ViewController vc = PlanningPoker.getViewController();
         Assert.assertSame("PlanningPoker", poker.getName());
         Assert.assertSame("PlanningPoker", poker.getTabs().get(0).getName());
-        MainView mv = vc.getMainView();
-        int before = mv.getTabCount();
+        final MainView mv = vc.getMainView();
+        final int before = mv.getTabCount();
         vc.addNewGameTab();
         Assert.assertEquals(before + 1, mv.getTabCount());
     }
