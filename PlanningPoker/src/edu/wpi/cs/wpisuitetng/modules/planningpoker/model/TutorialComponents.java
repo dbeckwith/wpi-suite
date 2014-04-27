f70bdadfb7c76cc83eeaa02e9f8a3f87e87b51e9
Sam Carlberg <sam.carlberg@gmail.com>
2014-04-27 16:29:46 -0400

    Adds framework for storing tutorials

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/TutorialComponents.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.model;
+
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author Team 9
+ * @version 1.0
+ */
+public class TutorialComponents {
+    
+    private static final ArrayList<TutorialPath> TUTORIALS = new ArrayList<>();
+    
+    /**
+     * Gets the tutorial path with the given name, or null if no tutorial with
+     * that name exists.
+     * 
+     * @param tutorialName
+     *        the name of the tutorial to retrieve
+     */
+    public static TutorialPath getTutorial(String tutorialName) {
+        for (TutorialPath t : TUTORIALS) {
+            if (t.getName().equals(tutorialName)) {
+                return t;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Gets the tutorial path for the given panel, or null if no tutorial is
+     * associated with that panel.
+     * 
+     * @param panel
+     *        the panel to get the tutorial for
+     */
+    public static TutorialPath getTutorial(JPanel panel) {
+        for (TutorialPath t : TUTORIALS) {
+            if (t.getPanel().equals(panel)) {
+                return t;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Adds the given tutorial to the list of tutorials
+     * 
+     * @param tutorial
+     *        the tutorial to add
+     * @return true if the tutorial was added to the list, otherwise returns
+     *         false
+     */
+    public static boolean addTutorial(TutorialPath tutorial) {
+        if (!TUTORIALS.contains(tutorial)) {
+            return TUTORIALS.add(tutorial);
+        }
+        return false;
+    }
+    
+}
