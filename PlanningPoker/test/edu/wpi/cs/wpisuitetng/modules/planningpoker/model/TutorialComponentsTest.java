7b72639dd8b3f3f6df144824288caf397466a027
Sam Carlberg <sam.carlberg@gmail.com>
2014-04-28 17:54:48 -0400

    Fixes TutorialComponentsTest

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/TutorialComponentsTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/TutorialComponentsTest.java
@@ -1,27 +1,24 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.model;
 
-import javax.swing.JPanel;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class TutorialComponentsTest {
     
     @Test
     public void testAddNamedTutorial() {
+        // creating a tutorial adds it to TutorialComponents
         TutorialPath tutorial = new TutorialPath("namedTutorial");
+        
+        // can't add it a second time, so this should return false
         assertFalse(TutorialComponents.getInstance().addTutorial(tutorial));
+        
+        // assert that it's been stored
         assertNotNull(TutorialComponents.getInstance().getTutorial("namedTutorial"));
-    }
-    
-    @Test
-    public void testAddTutorialWithPanel() {
-        JPanel panel = new JPanel();
-        TutorialPath tutorial = new TutorialPath("panelTutorial", panel);
-        assertFalse(TutorialComponents.getInstance().addTutorial(tutorial));
-        assertNotNull(TutorialComponents.getInstance().getTutorial("panelTutorial"));
-        assertNotNull(TutorialComponents.getInstance().getTutorial(panel));
+        
+        // make sure that the stored tutorial is the same as the one created
+        assertEquals(tutorial, TutorialComponents.getInstance().getTutorial("namedTutorial"));
     }
     
 }
